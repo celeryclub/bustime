8daee1208a47e2419a126a6bc82e65de62f20b33
Steve Davis <steve@celery.club>
2016-01-02 14:18:52 -0500

    Set up distutils configuration

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'bustime',
+  packages = ['bustime'],
+  version = '0.1',
+  description = 'SDK for the MTA Bus Time API',
+  author = 'Steve Davis',
+  author_email = 'steve@celery.club',
+  url = 'https://github.com/celeryclub/bustime',
+  download_url = 'https://github.com/celeryclub/bustime/tarball/0.1',
+  keywords = ['mta', 'bustime'],
+  classifiers = [],
+)
